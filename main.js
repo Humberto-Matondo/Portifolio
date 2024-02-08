91c1610343eec9ea1fcba8be64cab8fe26c9fb09
Humberto-Matondo <humbertomat99@gmail.com>
2024-02-08 17:57:27 +0100

    Add JS typed and css animation

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Software Developer", "Python Developer", "FullStack Developer"],
+    typeSpeedy: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
