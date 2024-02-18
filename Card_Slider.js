20e76155598e5f5f26da686b63b160c3c9f59a20
Humberto-Matondo <humbertomat99@gmail.com>
2024-02-18 13:46:23 +0100

    corrigindo bugs vol.3

--- Card_Slider.js
+++ Card_Slider.js
@@ -2,7 +2,7 @@ let items = document.querySelectorAll('.slider .item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 
-let active = 3; 
+let active = 3;
 
 function loadshow() {
     let stt = 0;
@@ -14,30 +14,29 @@ function loadshow() {
 
     for (let i = active + 1; i < items.length; i++) {
         stt++;
-        items[i].style.transform = 'translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)';
+        items[i].style.transform = 'translateX(' + 120 * stt + 'px) scale(' + (1 - 0.2 * stt) + ') perspective(16px) rotateY(-1deg)';
         items[i].style.zIndex = -stt;
         items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6; 
+        items[i].style.opacity = stt > 2 ? 0 : 0.6;
     }
 
     stt = 0;
     for (let i = active - 1; i >= 0; i--) {
         stt++;
-        items[i].style.transform = 'translateX(${-120*stt}px) scale(${ 1 - 0.2*stt}) perspective(16px) rotateY(1deg)';
+        items[i].style.transform = 'translateX(' + (-120 * stt) + 'px) scale(' + (1 - 0.2 * stt) + ') perspective(16px) rotateY(1deg)';
         items[i].style.zIndex = -stt;
         items[i].style.filter = 'blur(5px)';
         items[i].style.opacity = stt > 2 ? 0 : 0.6;
     }
 }
-
 loadshow();
 
 next.onclick = function () {
-    active = active + 1 < items.length ? active + 1 : 0; 
+    active = active + 1 < items.length ? active + 1 : 0;
     loadshow();
 };
 
 prev.onclick = function () {
-    active = active - 1 >= 0 ? active - 1 : items.length - 1; 
+    active = active - 1 >= 0 ? active - 1 : items.length - 1;
     loadshow();
 };
