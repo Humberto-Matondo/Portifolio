b44383d7c43389042e46fdb209247791b52b81d6
Humberto-Matondo <humbertomat99@gmail.com>
2024-03-10 22:35:21 +0100

    removi os testemunhos, e add nova cor e movimentacao.

--- assets/Js/scroolAnimation.js
+++ assets/Js/scroolAnimation.js
@@ -3,16 +3,18 @@ const sr = ScrollReveal(
         origin: 'top',
         distance: '60px',
         duration: 2500,
-        delay:400,
+        delay: 400,
         reset: true,
     }
 )
 
-sr.reveal('.about-img, .about-text', {interval:100})
-sr.reveal('.contact-text', {origin: 'left',})
-sr.reveal('.contact-form', {origin: 'right',})
-sr.reveal('.skill__container', {origin: 'bottom',})
-sr.reveal('.testimonial, .container-proj' , {interval:100})
+sr.reveal('.home-image', { origin: 'bottom', interval: 500 })
 
+sr.reveal('.about-img', { origin: 'right', })
+sr.reveal('.about-text', { origin: 'left', })
 
+sr.reveal('.skill__container .skills__container', { interval: 500 })
+sr.reveal('.container-proj .card-proj', { interval: 500 })
 
+sr.reveal('.contact-text', { origin: 'right', })
+sr.reveal('.contact-form', { origin: 'left', })
